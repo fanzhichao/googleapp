29136797610be320501a36b5f00c44ff3768df83
fanzhichao <fanzc@126.com>
2024-09-19 13:44:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -152,4 +152,25 @@ else
 initBalls();
 gameLoop();
 
+window.addEventListener("load", function () {
+    if(window.tableball)
+    {
+        console.log("---------------1111111111");
+    //   window.tableball.tball(null,"https://www.126.com");
+    //   window.tableball.jball();
+
+          window.tableball.tball("good",null);
+          window.tableball.jball();
+          console.log("---------------set cookie in java ok, begin to get value in js");
+          let cookies = document.cookie;
+          console.log(cookies);
+          window.tableball.tball("good",cookies);
+          window.tableball.tball("good","good");
+    }
+    else
+    {
+        console.log("---------------2222222222");
+    }
+});
+
 
